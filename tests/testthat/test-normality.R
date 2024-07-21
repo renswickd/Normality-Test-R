f587a2cab2fa2e4110add8f866d3be283914d963
renswickd <renswick.delver@gmail.com>
2024-07-22 11:08:40 +1200

    Warning Messages for NA values and unit test cases added

--- tests/testthat/test-normality.R
+++ tests/testthat/test-normality.R
@@ -2,5 +2,6 @@ test_that("normality function works", {
   expect_no_error(normality(rnorm(100)))
   expect_no_error(normality(rpois(100,3)))
   expect_error(normality(c("add")))
+  expect_warning(normality(c(1,2,3,4,NA)))
 })
   
