ad1c9cb007b88037cac9ef693c6b767caf0de440
renswickd <renswick.delver@gmail.com>
2024-07-25 13:25:20 +1200

    Unit test cases were updated for the dimension and data type exceptions

--- R/normality.R
+++ R/normality.R
@@ -1,20 +1,22 @@
 normality <- function(data, plot=FALSE) {
+
   # detect null values
-  if (all(is.null(data))) stop("Input argument has NULL values")
-  if (all(is.infinite(data))) stop("Input argument has Infinite values")
+  if (all(is.null(data))) stop("Input need to be a non NULL value")
+  if (all(is.infinite(data))) stop("Input need to be a non Inf value")
+  if (all(is.na(data))) stop("Input need to be a non NA value")
 
   #check argument-data
   ## all NA, other than numeric data
 
   stopifnot(is.numeric(data))
-  # print(data)
-  # print(length(dim(data)))
+
   if (length(dim(data)) > 1) stop("Input must be an 1-D vector/array")
   if (length(unique(data)) == 1) stop("Input must not be identical")
-  if (all(is.na(data)) || length(data) <=2 ) stop("Input must have atleast three or more non NA values")
+  if (length(data) <=2 ) stop("Input must have atleast three or more non NA values")
+
   ## warning for NA values
   if (any(is.na(data))) {
-    warning("DATA has NA values")
+    warning("DATA has NA values: W-test statistics value is derived ignoring NAs")
   }
 
   #check argument-plot
@@ -30,8 +32,8 @@ normality <- function(data, plot=FALSE) {
 }
 
 # sample_data <- c(1,2,3,4,5,6,2,3,4,3,4)
-# # is.numeric(sample_data)
-# normality(sample_data)
+# is.numeric(sample_data)
+# normality(NA)
 # normality(matrix(c(1,2,3)))
 #
 #
--- tests/testthat/test-normality.R
+++ tests/testthat/test-normality.R
@@ -16,15 +16,16 @@ test_that("normality function works", {
   expect_no_error(normality(rnorm(100), plot = FALSE))
   expect_no_error(normality(rpois(100,3), plot = FALSE))
 
-  expect_error(normality(NULL))
-  expect_error(normality(NA))
-  expect_error(normality(Inf))
+  expect_error(normality(NULL),"Input need to be a non NULL value")
+  expect_error(normality(NA), "Input need to be a non NA value")
+  expect_error(normality(Inf), "Input need to be a non Inf value")
+
+  expect_error(normality(array(c(1,2,3,4),dim = c(2,2))), "Input must be an 1-D vector/array")
+  expect_error(normality(c(7,7,7,7,7)), "Input must not be identical")
   expect_error(normality(c("incorrect input format")))
-  expect_error(normality(c(1,99)))
-  expect_error(normality(array(c(1,2,3,4),dim = c(2,2))))
+  expect_error(normality(c(1,99)),"Input must have atleast three or more non NA values")
 
-  expect_warning(normality(c(1,2,3,4,NA)))
+  expect_warning(normality(c(1,2,3,4,NA)), "DATA has NA values: W-test statistics value is derived ignoring NAs")
 
-  # expect_error(normality(rnorm(100),plot=NULL))
-  expect_error(normality(rnorm(100),plot=c("random")))
+  expect_error(normality(rnorm(100),plot=c("random")), "Paramete plot must have Boolean values")
 })
